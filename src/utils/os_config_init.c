4b072257fbf5d187983cc271452a9c59622c3bc0
amber_lizi <amberxl1126@outlook.com>
2016-08-10 17:22:26 -0700

    change

--- src/utils/os_config_init.c
+++ src/utils/os_config_init.c
@@ -597,7 +597,7 @@ static GF_Config *create_default_config(char *file_path)
 	gf_cfg_set_key(cfg, "Video", "HardwareMemory", "Auto");
 	gf_cfg_set_key(cfg, "Network", "AutoReconfigUDP", "yes");
 	gf_cfg_set_key(cfg, "Network", "UDPTimeout", "10000");
-	gf_cfg_set_key(cfg, "Network", "BufferLength", "3000");
+	gf_cfg_set_key(cfg, "Network", "BufferLength", "30000");
 
 
 	/*locate GUI*/
