175c047d06d185d4d94200b4dbf58f3d1619d891
amber_lizi <amberxl1126@outlook.com>
2016-08-12 10:10:46 -0700

    uncompiled modified gpac

--- build/android/obj/local/x86/objs/javaenv/__/__/__/__/modules/droid_audio/javaenv.o.d
+++
@@ -1,5 +0,0 @@
-/Users/amberxiangli/gpac-android/build/android/obj/local/x86/objs/javaenv/__/__/__/__/modules/droid_audio/javaenv.o: \
- /Users/amberxiangli/gpac-android/build/android/jni/modules/../../../../modules/droid_audio/javaenv.c \
- /Users/amberxiangli/gpac-android/build/android/jni/modules/../../../../modules/droid_audio/javaenv.h
-
-/Users/amberxiangli/gpac-android/build/android/jni/modules/../../../../modules/droid_audio/javaenv.h:
