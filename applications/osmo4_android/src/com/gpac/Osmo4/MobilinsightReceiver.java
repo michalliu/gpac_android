853b9ffde88cc4f9c0f8b15b23460309a4137abd
Yuanjie Li <yuanjie.li@cs.ucla.edu>
2016-08-15 11:53:19 -0700

    update predicted bandwidth

--- applications/osmo4_android/src/com/gpac/Osmo4/MobilinsightReceiver.java
+++ applications/osmo4_android/src/com/gpac/Osmo4/MobilinsightReceiver.java
@@ -14,14 +14,17 @@ public class MobilinsightReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		String phy_bandwidth_char = intent.getStringExtra("Bandwidth (Mbps)");
+		String pred_phy_bandwidth_char = intent.getStringExtra("Predicted Bandwidth (Mbps)");
 		String prompt = "phy bandwidth = ";
 		String unit = "Mbps";
 		String toast = prompt + phy_bandwidth_char + unit;
 		//Toast.makeText(context, toast, Toast.LENGTH_LONG).show();
 		
 		float phy_bandwidth = Float.parseFloat(phy_bandwidth_char);
+		float pred_phy_bandwidth = Float.parseFloat(pred_phy_bandwidth_char);
 
-		setPHYBandwidth(phy_bandwidth);	
+		// setPHYBandwidth(phy_bandwidth);	
+		setPHYBandwidth(pred_phy_bandwidth);	// predicted bandwidth
 	}
 	
 	public native void setPHYBandwidth(float bw);
